c8850ee35070613eb33e655fe9d21e1e3800aa7b
crack <crack>
2019-04-30 00:33:15 +0900

    make calculateBlockHash & get Block methods

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,15 @@ class Block {
     public previousHash: string;
     public data: string;
     public timestamp: number;
+
+    static calculateBlockHash = (
+        index:number,
+        previousHash: string,
+        timestamp: number,
+        data: string
+    ): string => 
+        CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+
     constructor(
         index: number,
         hash: string,
@@ -23,8 +32,12 @@ class Block {
 
 const genesisBlock: Block = new Block(0, "2020202020", "", "Hello", 123456);
 
-let blockchain: [Block] = [genesisBlock];
+let blockchain: Block[] = [genesisBlock];
 
-console.log(blockchain);
+const getBlockchain = (): Block[] => blockchain;
+
+const getLatestBlock = (): Block => blockchain[blockchain.length -1];
+
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
 
 export {};

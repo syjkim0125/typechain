3dca86a30a049ec01a9b837ed654fc4ced597c33
crack <crack>
2019-04-30 00:08:30 +0900

    create Block class

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,30 @@
-interface Human {
-    name: string,
-    age: number,
-    gender: string
+import * as CryptoJS from "crypto-js";
+
+class Block {
+    public index: number;
+    public hash: string;
+    public previousHash: string;
+    public data: string;
+    public timestamp: number;
+    constructor(
+        index: number,
+        hash: string,
+        previousHash: string,
+        data: string,
+        timestamp: number
+    ) {
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timestamp = timestamp;
+    }
 }
 
-const person = {
-    name: "Tora",
-    age: 20,
-    gender: "male"
-}
+const genesisBlock: Block = new Block(0, "2020202020", "", "Hello", 123456);
 
-const sayHi = (person: Human): string => {
-    return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
-};
+let blockchain: [Block] = [genesisBlock];
 
-console.log(sayHi(person));
+console.log(blockchain);
 
 export {};
